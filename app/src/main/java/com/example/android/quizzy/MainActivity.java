e2f6e1396f134207d00a8b0a20171f5eec486cdc
mesibaja <petra.lehoczky@gmail.com>
2017-04-11 10:45:07 +0200

    java updates: question 4 inserted

--- app/src/main/java/com/example/android/quizzy/MainActivity.java
+++ app/src/main/java/com/example/android/quizzy/MainActivity.java
@@ -58,34 +58,22 @@ public class MainActivity extends AppCompatActivity {
             numberOfCorrectAnswers++;
         }
 
-        // Evaluate Question 2
-        // this is how you will evaluate multiple answers/checkboxes. Here is a help for more
-        // checkboxes:
-        //
-        // if (mQuestion2CorrectAnswer1CheckBox.isChecked() &&
-        //        mQuestion2CorrectAnswer2CheckBox.isChecked() &&
-        //        !mQuestion2IncorrectAnswer1CheckBox.isChecked() &&
-        //        !mQuestion2IncorrectAnswer2CheckBox.isChecked()) {
-        //    numberOfCorrectAnswers++;
-        // }
         if (!mQuestion2IncorrectAnswer1CheckBox.isChecked() &&
                 mQuestion2CorrectAnswer1CheckBox.isChecked() &&
                 mQuestion2CorrectAnswer2CheckBox.isChecked()) {
             numberOfCorrectAnswers++;
         }
 
-        // Evaluate Question 3
-        // This is the same as in case of CheckBox, but you can only have 1 good answer (RadioButton)
         if (mQuestion3CorrectAnswerRadioButton.isChecked()) {
             numberOfCorrectAnswers++;
         }
 
-        // Evaluate the user based on the number of his/her correct answers
-        // I tried to make it as simple as possible, but you need to follow some rules when you
-        // modify this:
-        //   1. You need to keep it ASC order. I.e. smallest numbers are on the top.
-        //   2. Actually, I am not sure if there are any other rules. I can help, if something
-        //      doesn't work.
+        String question4Answer = mQuestion1AnswerEditText.getText().toString();
+        // Check if it is equals to the correct answer that you store in the strings.xml
+        if (question1Answer.equals(getString(R.string.question4_correct_answer))) {
+            numberOfCorrectAnswers++;
+        }
+
         if (numberOfCorrectAnswers < 2) {
             Toast.makeText(this, "Not bad but you need more practice in order to stay healthy.", Toast.LENGTH_LONG).show();
         } else if (numberOfCorrectAnswers < 3) {
